2f9fbb2916d6fd2c4b81bcdc96f4c17371783664
Anuja-star <anujachavan8641@gmail.com>
2025-02-11 09:30:00 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,6 +1,6 @@
 x=int(input("Enter a first number="))
 y=int(input("Enter a second number="))
 add=x+y;
-sub=x-y"
-print("Addidtion of two numbers is :",add)
+sub=x-y;
+print("Addition of two numbers is :",add)
 print("Substraction of two numbers is :",sub)
